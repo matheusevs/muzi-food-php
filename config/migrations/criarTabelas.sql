70c679a057a55a53fa014a03c31443cff954c159
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2024-09-13 12:21:09 -0300

    refactor: Modificando consultas

--- config/migrations/criarTabelas.sql
+++ config/migrations/criarTabelas.sql
@@ -1,46 +1,53 @@
 CREATE DATABASE IF NOT EXISTS Muzi_Food;
 USE Muzi_Food;
 
-CREATE TABLE IF NOT EXISTS usuarios
-(
-    id            INT AUTO_INCREMENT PRIMARY KEY,
-    nome          VARCHAR(100) NOT NULL,
-    email         VARCHAR(100) NOT NULL UNIQUE,
-    senha         VARCHAR(255) NOT NULL,
-    data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS clientes
-(
-    id            INT AUTO_INCREMENT PRIMARY KEY,
-    nome          VARCHAR(100) NOT NULL,
-    email         VARCHAR(100) NOT NULL UNIQUE,
-    senha         VARCHAR(255) NOT NULL,
-    telefone      VARCHAR(15),
-    data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS produtos
-(
-    id            INT AUTO_INCREMENT PRIMARY KEY,
-    nome          VARCHAR(100)   NOT NULL,
-    descricao     TEXT,
-    imagem        TEXT,
-    preco         DECIMAL(10, 2) NOT NULL,
-    data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    id_usuario    INT,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
-);
-
-CREATE TABLE IF NOT EXISTS pedidos
+CREATE TABLE IF NOT EXISTS users
 (
     id          INT AUTO_INCREMENT PRIMARY KEY,
-    id_cliente  INT,
-    id_produto  INT,
-    endereco    TEXT,
-    data_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_cliente) REFERENCES clientes (id),
-    FOREIGN KEY (id_produto) REFERENCES produtos (id)
+    name        VARCHAR(100) NOT NULL,
+    email       VARCHAR(100) NOT NULL UNIQUE,
+    password    VARCHAR(255) NOT NULL,
+    record_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-Alter table produtos add column imagem text after descricao;
+CREATE TABLE IF NOT EXISTS clients
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL,
+    email       VARCHAR(100) NOT NULL UNIQUE,
+    password    VARCHAR(255) NOT NULL,
+    phone       VARCHAR(15),
+    record_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS products
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(100)   NOT NULL,
+    description TEXT,
+    image       TEXT,
+    price       DECIMAL(10, 2) NOT NULL,
+    record_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_user     INT,
+    FOREIGN KEY (id_user) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS orders
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    id_client  INT,
+    id_product INT,
+    address    TEXT,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_client) REFERENCES clients (id),
+    FOREIGN KEY (id_product) REFERENCES products (id)
+);
+
+CREATE TABLE IF NOT EXISTS contacts
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(100) NOT NULL,
+    email       VARCHAR(100) NOT NULL UNIQUE,
+    phone       VARCHAR(15),
+    message     TEXT
+);
