698ec8b46728f638beef376490e5a4c8070f64eb
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2024-09-17 16:31:55 -0300

    feat: Criando conexão com o banco de dados

--- app/models/Clients.php
+++ app/models/Clients.php
@@ -1,11 +1,16 @@
 <?php
 
-require_once '../../config/database/ConnectDb.php';
+require_once __DIR__ . '/../../config/database/ConnectDb.php';
 
 class Clients {
 
+    private $con;
+    private $connect;
+
     public function __construct()
     {
+        $this->con = new ConnectDb();
+        $this->connect = $this->con->connection();
     }
 
 }
--- app/models/Contacts.php
+++ app/models/Contacts.php
@@ -1,11 +1,16 @@
 <?php
 
-require_once '../../config/database/ConnectDb.php';
+require_once __DIR__ . '/../../config/database/ConnectDb.php';
 
 class Contacts {
 
+    private $con;
+    private $connect;
+
     public function __construct()
     {
+        $this->con = new ConnectDb();
+        $this->connect = $this->con->connection();
     }
 
 }
