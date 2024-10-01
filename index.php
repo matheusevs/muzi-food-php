9138863da6ab9672ae584b7e9ed57eaca06b4cec
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2024-10-01 16:59:48 -0300

    feat: Alterando nome do projeto

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Enzo Food - Matheus Eduardo</title>
     <!-- Fonte -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
