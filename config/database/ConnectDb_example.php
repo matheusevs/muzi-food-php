07aaa61f49f1688684412cf57ccb432a63308526
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2024-10-01 16:51:52 -0300

    feat: Adicionando o arquivo de exemplo do database

---
+++ config/database/ConnectDb_example.php
@@ -0,0 +1,31 @@
+
+<?php
+
+class ConnectDb{
+
+    public $host;
+    public $user;
+    public $pass;
+    public $data;
+
+
+    public function __construct()
+    {
+        $this->host = 'localhost'; // host do seu banco de dados
+        $this->user = 'root'; // user do seu banco de dados
+        $this->pass = ''; // password do seu banco de dados
+        $this->data = 'Muzi_Food'; // nome do database, conforme criado em migration
+    }
+
+    public function connection()
+    {
+        $connection = mysqli_connect($this->host, $this->user, $this->pass, $this->data);
+        if(!$connection){
+            return ['error' => mysqli_connect_error()];
+        }
+
+        return $connection;
+
+    }
+
+}
